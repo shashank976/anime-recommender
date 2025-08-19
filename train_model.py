4ddd2ef5f0b28474bad723820bb1ca0e28217a19
Shashank Sambaru <ssambaru87@gmail.com>
2025-08-19 12:36:57 -0400

    Fixing data so I can retrain model

--- train_model.py
+++ train_model.py
@@ -7,8 +7,6 @@ import joblib
 
 df_anime = pd.read_csv('anime-filtered.csv', usecols=['Name', 'English name', 'Genres', 'Score', 'Episodes', 'sypnopsis', 'Type'])
 
-scaler = MinMaxScaler()
-df_anime['score'] = scaler.fit_transform(df_anime[['Score']])
 valid_types = ['TV', 'Movie', 'ONA']
 df_anime = df_anime[df_anime['Type'].isin(valid_types)].drop_duplicates(subset='Name')
 
@@ -25,14 +23,13 @@ def valid_episode_count(row):
 #making sure needed features are valid
 df_anime = df_anime[df_anime.apply(valid_episode_count, axis=1)]
 df_anime['Episodes'] = df_anime['Episodes'].astype(int)
-df_anime_scored = df_anime.dropna(subset=['Score'])
 df_anime = df_anime[df_anime['Score'].apply(lambda x: str(x).replace('.', '', 1).isdigit() and float(x) > 6.0)]
 df_anime['Score'] = df_anime['Score'].astype(float)
 df_anime = df_anime.dropna(subset=['Genres'])
 df_anime['English name'] = df_anime['English name'].fillna(df_anime['Name'])
 df_anime['sypnopsis'] = df_anime['sypnopsis'].fillna('')
 df_anime['genre_str'] = df_anime['Genres'].apply(lambda x: ' '.join(x.split(', ')))
-df_anime['combined_features'] = df_anime['genre_str'] + ' ' + df_anime['sypnopsis']
+df_anime['combined_features'] = (df_anime['genre_str'] + ' ')*3 + df_anime['sypnopsis']
 df_anime = df_anime.reset_index(drop=True)
 
 
